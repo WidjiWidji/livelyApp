ef211ed1e28e76d2d9ef67b1aa4954d0fb64cc55
Karsten Widjanarko <karsten.widjanarko@gmail.com>
2019-04-27 12:03:17 -0700

    Initial Commit

---
+++ livelyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  livelyApp
+//
+//  Created by Karsten Widjanarko on 4/27/19.
+//  Copyright © 2019 drunkBrothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
