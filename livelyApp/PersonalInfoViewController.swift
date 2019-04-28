2808ccf939755fa1800ef676a3eabc8bb0cbf0e3
Karsten Widjanarko <karsten.widjanarko@gmail.com>
2019-04-28 02:10:49 -0700

    save button, bmi calculation. needs to fix data saving and kept on personal info

--- livelyApp/PersonalInfoViewController.swift
+++ livelyApp/PersonalInfoViewController.swift
@@ -36,20 +36,68 @@ class PersonalInfoViewController: UIViewController {
     @IBOutlet weak var workoutSlider: UISlider!
     
     
-    @IBAction func workoutVolSlider(_ sender: AnyObject) {
-        workoutLabel.text = String(sender.value)
+    @IBAction func workoutVolSlider(_ sender: UISlider) {
+        workoutLabel.text = String(Int(sender.value))
     }
+    
+    //BMI = kg/m2 (weight/height^2)
+    func bmiCalc() -> Double{
+        let weight = weightField.text!
+        let heightFt = heightFtField.text!
+        let heightIn = heightInField.text!
+        
+        let weightInKilogram:Double = (Double(weight))! / 2.205
+        let heightInMeters:Double = (Double(heightFt)! / 3.2808) + (Double(heightIn)! * 0.0254)
+        
+        return weightInKilogram / (heightInMeters * heightInMeters)
+    }
+    
+    
+    //alert
+    func createAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func calcBmiButton(_ sender: UIButton) {
+        if(weightField.text == "") //if weight is empty
+        {
+            createAlert(title: "Cannot Calculate BMI" , message: "Fill in weight")
+        }
+        else if (heightFtField.text == "") //if height ft is empty
+        {
+            createAlert(title: "Cannot Calculate BMI", message: "Fill in height (Ft)")
+        }
+        else if (heightInField.text == "") //if height in is empty
+        {
+            heightInField.text = String(0)
+        }
+        else
+        {
+            bmiLabel.text = String(Int(bmiCalc()))
+        }
+    }
+    
+    //saving all personal info as userDefaults
     @IBAction func saveButton(_ sender: UIButton) {
+        
         UserDefaults.standard.set(genderControl.selectedSegmentIndex, forKey: "userGender")
         UserDefaults.standard.set(ageField.text, forKey: "userAge")
         UserDefaults.standard.set(heightFtField.text, forKey: "userHeightFt")
         UserDefaults.standard.set(heightInField.text, forKey: "userHeightIn")
         UserDefaults.standard.set(weightField.text, forKey: "userWeight")
+        UserDefaults.standard.set(bmiLabel.text, forKey: "userBmi")
+        UserDefaults.standard.set(workoutLabel.text, forKey: "userWorkoutVol")
         
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.HideKeyboard()
+        //self.HideKeyboard()
         
 
         // Do any additional setup after loading the view.
@@ -58,6 +106,30 @@ class PersonalInfoViewController: UIViewController {
         if let gender = UserDefaults.standard.object(forKey: "userGender") as? Int
         {
             genderControl.selectedSegmentIndex = gender
+        }
+        if let age = UserDefaults.standard.object(forKey: "userAge") as? Int
+        {
+            ageField.text = String(age)
+        }
+        if let heightFeet = UserDefaults.standard.object(forKey: "userHeightFt") as? Int
+        {
+            heightFtField.text = String(heightFeet)
+        }
+        if let heightInches = UserDefaults.standard.object(forKey: "userHeightIn") as? Int
+        {
+            heightInField.text = String(heightInches)
+        }
+        if let weight = UserDefaults.standard.object(forKey: "userWeight") as? Int
+        {
+            weightField.text = String(weight)
+        }
+        if let bmi = UserDefaults.standard.object(forKey: "userBmi") as? Int
+        {
+            bmiLabel.text = String(bmi)
+        }
+        if let workoutVolume = UserDefaults.standard.object(forKey: "userWorkoutVol") as? Int
+        {
+            workoutLabel.text = String(workoutVolume)
         }
     }
     
